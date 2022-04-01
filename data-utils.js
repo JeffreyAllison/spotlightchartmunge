6c8b79c6440e6a416ec270e31e6052d73dfa1750
JeffAllison <itsjallison@gmail.com>
2022-04-01 10:09:47 -0700

    added purchaseFrequencyCountMap function in data.js, added corresponding test to example.test.js, added table to app.js

---
+++ data-utils.js
@@ -0,0 +1,22 @@
+export function purchaseFrequencyCountMap (customers) {
+
+  const countMap = {};
+
+  for (let customer of customers) {
+
+    const frequency = customer.purchase_frequency;
+
+    if (countMap[frequency]) {
+
+      countMap[frequency]++;
+
+    } else {
+
+      countMap[frequency] = 1;
+    }
+  }
+  return countMap;
+
+
+
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,40 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { purchaseFrequencyCountMap } from '../data-utils.js';
+import { customers } from '../data.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+  //Arrange
+  // Set up your arguments and expectations
+  const expected = true;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+  //Act 
+  // Call the function you're testing and set the result to a const
+  const actual = true;
+
+  //Expect
+  // Make assertions about what is expected versus the actual result
+  expect.equal(actual, expected);
+});
+
+test('receives customer data and returns purchase frequency count map', (expect) => {
+
+  const countMap = {
+    'Daily': 142,
+    'Monthly': 123,
+    'Never': 123,
+    'Often': 126,
+    'Once': 107,
+    'Seldom': 129,
+    'Weekly': 122,
+    'Yearly': 128
+
+  };
+
+  const expected = countMap;
+
+  const actual = purchaseFrequencyCountMap(customers);
+
+  expect.deepEqual(actual, expected);
+
 });
